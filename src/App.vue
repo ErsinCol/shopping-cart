28818c656259c69d2f90333b1bf201571548f0ac
Ersin Çöl <ersincol235@gmail.com>
2023-12-28 11:14:46 +0300

    create project with vite, create product list component, create simulated API

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ProductList from "@/components/ProductList.vue";
+</script>
+
+<template>
+  <header>
+    <h1>Shopping Card App</h1>
+  </header>
+
+  <main>
+    <ProductList />
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import shop from "@/api/shop.js";
+
+const products = ref([]);
+
+onMounted(()=>{
+  shop.getProducts((productsArr)=>{
+    products.value = productsArr;
+  })
+})
+</script>
+
+<template>
+  <div>
+    <h2>Product List</h2>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{product.title}} - {{product.price}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
